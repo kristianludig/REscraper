475fea5a0e6d1dae388cb87c55a5c896572eae68
Kristian Ludig <kristian.ludig@gmail.com>
2017-01-25 17:40:49 -0500

    completed inital Metcap parser

--- REscraper/spiders/test_crawl.py
+++ REscraper/spiders/test_crawl.py
@@ -21,11 +21,11 @@ class REscraper(scrapy.Spider):
                 next_page = unit.xpath('.//a/@href').extract_first()
                 yield scrapy.Request(next_page, callback=self.parse_metcap)
         #Briar Lane Scraper
-        '''if 'briarlane' in response.url:
+        if 'briarlane' in response.url:
             for unit in response.xpath('//div[@id="tab1"]/div[@class="span6"]/div/div[@class="span6"]'):
                 if unit.xpath('.//a[@class="btn btn-info"]/@href').extract_first() is not None:
                     next_page = unit.xpath('.//a[@class="btn btn-info"]/@href').extract_first()
-                    yield {'link': response.urljoin(next_page)}'''
+                    yield {'link': response.urljoin(next_page)}
 
         '''page = response.url.split("/")[-2]
         filename = 'units-metcap.html'
@@ -33,26 +33,33 @@ class REscraper(scrapy.Spider):
             f.write(response.body)
         self.log("saved file %s" % filename)'''
 
+#scrapes available units from Metcap Toronto and outputs address, bedrooms, and rent for each unit
+#at each address
     def parse_metcap(self, response):
-        load = ItemLoader(item=Unit())
         units = []
         address = response.xpath('//p[@class="address"]/text()').extract_first().strip()
         bedrooms = response.xpath('//td[@class="col-beds"]/text()').re(r'(Studio|\d)')
         rents = response.xpath('//td[@class="col-rent-from"]/text()').re(r'(\$\d+,*\d*)')
-        '''info = "Address:", address
-        for unit in bedrooms:
-            if bedrooms:
-                info = info, "Bedrooms:", unit, "Rent:", rents.pop()
-                #print info'''
-        load.replace_value("address", address)
+
         if not bedrooms:
+            load = ItemLoader(item=Unit())
+            load.replace_value("address", address)
             units.append(load.load_item())
         else:
             for unit in bedrooms[:]:
+                load = ItemLoader(item=Unit())
+                load.replace_value("address", address)
                 load.replace_value("bedrooms", bedrooms.pop())
                 if rents:
                     load.replace_value("rent", rents.pop())
                 units.append(load.load_item())
 
         return units
+
+    def parse_briarlane(self, response):
+        units = []
+        address = response.xpath('//h2/text()').extract_first().strip()
+        #note: try sorting by tr instead of td - too many different types of unit names
+        bedrooms = response.xpath('//table[@class="table table-striped"]/tbody/td/text()').re(r'(Bachelor|Studio|One|Two|Three|Four)')
+        rents = response.xpath()
 
